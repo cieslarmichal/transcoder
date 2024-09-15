7b17a6b4273a98122b3c774615c3abf3501fabad
Michal Cieslar <michalovskyyy@gmail.com>
2024-09-15 12:45:32 +0200

    fix: fix validation patterns

--- common/contracts/src/amqp/messages/validationPatterns.ts
+++ common/contracts/src/amqp/messages/validationPatterns.ts
@@ -2,4 +2,4 @@ export const uuidPattern = '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-
 
 export const urlPattern = '^https:\\/\\/[^\\s/$.?#].[^\\s]*$';
 
-export const pathPattern = '^\\/[^\\/]+$';
+export const pathPattern = '^\\/.*$';
